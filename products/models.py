b656c7567262c059e259213f202e3e90966566cc
owais azad <azadm1808@gmail.com>
2023-03-09 13:26:11 +0500

    Creat a new Model Named Islamic Academy Products in side Product app

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-#FOR PRODUCT:
+
+#FOR AL-NAFI MAIN SITE PRODUCT:
 class AlnafiProduct(models.Model):
     # image = models.ImageField(null=True, blank=True,
     #                           upload_to='media/products', default="https://import.cdn.thinkific.com/212959/IbhpMoPT4WPwllrOflEp_logo_transperant_png")
@@ -50,3 +51,31 @@ class AlnafiProduct(models.Model):
     class Meta:
         managed = True
         verbose_name = "Al-Nafi Product"
+
+
+#FOR ISLAMIC ACADEMY PRODUCT:
+class IslamicAcademyProduct(models.Model):
+    TYPE_CHOICES = (
+        ('simple', 'Simple'),
+        ('variable', 'Variable'),
+    )
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('publish', 'Publish'),
+    )
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES, default='simple')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    regular_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    stock_status = models.CharField(max_length=20, default='instock')
+
+    class Meta:
+        managed = True
+        verbose_name = "Islamic Academy Product"
--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import AlnafiProduct
+from .models import IslamicAcademyProduct
 from rest_framework.serializers import ModelSerializer
 
-#FOR PRODUCT:
+#FOR AL-NAFI MAIN SITE PRODUCT:
 class ProductSerializer(ModelSerializer):
     def __init__(self, data, fields="__all__", action="serialize"):
         self.Meta.fields = fields  # type: ignore
@@ -13,3 +14,10 @@ class ProductSerializer(ModelSerializer):
     class Meta:
         model = AlnafiProduct
         fields = "__all__"
+
+
+#FOR ISLAMIC ACADEMY PRODUCT:
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IslamicAcademyProduct
+        fields = ("id", "name", "slug", "date_created", "date_modified", "type", "status", "description", "price", "regular_price", "sale_price", "stock_status")
