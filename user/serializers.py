87e126a2bfa1d2b96faae6e90262fddc016cd005
owais azad <azadm1808@gmail.com>
2023-03-10 19:45:54 +0500

    add Serializer for Isalmic Academy User model

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import User
+from .models import IslamicAcademyUser
 
+
+# For Main Site Al-Nafi User Table:
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+# For Islamic Academy Users:
+
+class IslamicAcademyUserSerializer(serializers.ModelSerializer):
+    address = serializers.JSONField()
+
+    class Meta:
+        model = IslamicAcademyUser
         fields = '__all__'
