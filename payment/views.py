e8cd11d432ad1d42780d848c6ce92615b42e0115
faizan1487 <faizanahmed14887@gmail.com>
2023-03-17 20:22:45 +0500

    user payment detail api updated

--- payment/views.py
+++ payment/views.py
@@ -10,25 +10,20 @@ class GetUserDetails(APIView):
 
     def post(self,request):
         email = request.data['email']
-        source = request.data['source']
         
-        if source == 'easypaisa':
-            obj = easypaisa_payment(email)
-            serializer = Easypaisa_PaymentsSerializer(obj,many=True)
-        elif source == 'stripe':
-            obj = stripe_payment(email)
-            serializer = PaymentSerializer(obj,many=True)
-        else:
-            print('ubl')
-            obj = ubl_payment(email)
-            serializer = Ubl_Ipg_PaymentsSerializer(obj,many=True)
-            
-            
-        # obj = Payment.objects.filter(email=email,source=source)
-        print(obj)
-        # serializer = Easypaisa_PaymentsSerializer(obj,many=True)
-        return Response(serializer.data)
-    
-    
-    
-    
+        easypaisa_obj = easypaisa_payment(email)
+        easypaisa_serializer = Easypaisa_PaymentsSerializer(easypaisa_obj,many=True)
+        
+        stripe_obj = stripe_payment(email)
+        stripe_serializer = PaymentSerializer(stripe_obj,many=True)
+        
+        ubl_obj = ubl_payment(email)
+        ubl_serializer = Ubl_Ipg_PaymentsSerializer(ubl_obj,many=True)
+        
+        data = {
+            'easypaisa': easypaisa_serializer.data,
+            'stripe': stripe_serializer.data,
+            'ubl': ubl_serializer.data
+        }
+        
+        return Response(data)
