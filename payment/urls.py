f7214717768535f2299a2ef386c03b0e75c3cd9b
faizan1487 <faizanahmed14887@gmail.com>
2023-03-18 14:38:23 +0500

    User payment search apis created based on email and payment gateway

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,12 @@
 
 from django.urls import path
-from .views import GetUserDetails 
+from .views import GetUserDetails, GetEasyPaisaUserDetails, GetStripeUserDetails, GetUblUserDetails, GetAllUserDetails, SearchUser
+
 urlpatterns = [
-    path("",GetUserDetails.as_view(),name='get_users_details')
+    path("",GetAllUserDetails.as_view(),name='get_all_users_details'),
+    path("/payment",GetUserDetails.as_view(),name='get_user_details'),
+    path("/easypaisa",GetEasyPaisaUserDetails.as_view(),name='get_easypaisa_users_details'),
+    path("/stripe",GetStripeUserDetails.as_view(),name='get_stripe_users_details'),
+    path("/ubl",GetUblUserDetails.as_view(),name='get_ubl_users_details'),
+    # path('/search/', SearchUser.as_view()),
 ]
