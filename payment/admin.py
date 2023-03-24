101ebb3e49bf9c9b70852804d4efedde777b874b
owais azad <azadm1808@gmail.com>
2023-03-25 00:23:36 +0500

    In Payments APP All payments Models update and modified naming convntion and manages and field and admin.py register of model also

--- payment/admin.py
+++ payment/admin.py
@@ -1,32 +1,24 @@
 from django.contrib import admin
-from .models import Payment
+from .models import Stripe_Payment
 from .models import Easypaisa_Payment
 from .models import UBL_IPG_Payment
 from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
-
-
-# class PaymentAdmin(admin.ModelAdmin):
-#     list_display = ('payment_source', 'first_name', 'last_name', 'email', 'enrollment_name', 'enrollment_creation_date', 'enrollment_expiry_date', 'payment_date')
-#     per_page = 500
-#     search_fields = ('payment_souce','email')
-
-# admin.site.register(Payment,PaymentAdmin)     
+# Register your models here.    
 
 #For Stripe Payments:
-class PaymentAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    list_display = ('payment_id','name', 'email', 'phone', 'product', 'amount', 'created', 'status', 'currency', 'source', 'description', 'address')
-    per_page = 500
-    search_fields = ('email',)
+class StripePaymentAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('payment_id','name', 'customer_email', 'phone', 'product_name', 'amount', 'order_datetime', 'status', 'currency', 'source', 'description', 'address')
+    search_fields = ('customer_email','payment_id','name', 'phone','product_name')
+    list_filter = ('order_datetime', 'status', 'currency', 'source')
 
-admin.site.register(Payment,PaymentAdmin)     
+admin.site.register(Stripe_Payment,StripePaymentAdmin)     
 
 #For Easypaisa Payments:
 class EasypaisaPaymentsAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    list_display = ['ops_id','product_name','order_id','transaction_id','order_datetime','customer_msidn','customer_email','amount_pkr','status','source','credit_card','bin_bank_name','fee_pkr',	'fed_pkr','error_reason','token_paid_datetime']
-    per_page = 500
-    search_fields = ('customer_email',)
+    list_display = ['ops_id','product_name','order_id','transaction_id','order_datetime','customer_msidn','customer_email','amount','status','source','credit_card','bin_bank_name','fee_pkr',	'fed_pkr','error_reason','token_paid_datetime']
+    search_fields = ('customer_email','ops_id', 'product_name', 'order_id', 'transaction_id', 'customer_msidn', 'credit_card')
+    list_filter = ('order_datetime', 'status','source', 'token_paid_datetime')
 
 admin.site.register(Easypaisa_Payment, EasypaisaPaymentsAdmin)
 
@@ -34,7 +26,7 @@ admin.site.register(Easypaisa_Payment, EasypaisaPaymentsAdmin)
 #For UBL IPG Payments:
 class UBLIPGPaymentsAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ['transaction_id', 'customer_email', 'card_mask', 'product_name', 'order_datetime', 'order_id', 'amount', 'captured', 'reversed', 'refund', 'approval_code', 'source', 'status']
-    per_page = 500
-    search_fields = ('customer_email','card_mask',)
+    search_fields = ('customer_email','card_mask', 'transaction_id','product_name', 'order_id', 'approval_code')
+    list_filter = ('order_datetime', 'source', 'status')
 
 admin.site.register(UBL_IPG_Payment, UBLIPGPaymentsAdmin)   
--- payment/models.py
+++ payment/models.py
@@ -2,29 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-# class Payment(models.Model):
-#     payment_source = models.CharField(max_length=50)
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     email = models.EmailField()
-#     enrollment_name = models.CharField(max_length=50)
-#     enrollment_creation_date = models.CharField(max_length=80,null=True,blank=True)
-#     enrollment_expiry_date = models.CharField(max_length=80,null=True,blank=True)
-#     payment_date = models.CharField(max_length=80,null=True,blank=True) 
-
-#     def __str__(self):
-#         return self.email
-
-
-
-class Payment(models.Model):
+#For Stripe_Payment:
+class Stripe_Payment(models.Model):
     payment_id = models.CharField(max_length=100 , null=True , blank=True)
     name = models.CharField(max_length=50, null=True , blank=True)
-    email = models.EmailField()
+    customer_email = models.EmailField(null=True,blank=True)
     phone = models.CharField(max_length=50, null=True , blank=True)
     product = models.CharField(max_length=100, null=True , blank=True)
     amount = models.CharField(max_length=50, null=True , blank=True)
-    created = models.DateTimeField(max_length=60, null=True , blank=True)
+    order_datetime = models.DateTimeField(max_length=60, null=True , blank=True)
     status = models.CharField(max_length=50, null=True , blank=True)
     currency = models.CharField(max_length=50, null=True , blank=True)
     source = models.CharField(max_length=50, null=True , blank=True)
@@ -32,13 +18,13 @@ class Payment(models.Model):
     address = models.CharField(max_length=300, null=True , blank=True)
         
     def __str__(self):
-        return self.email
+        return self.customer_email
+    
     class Meta:
-        
-        managed = False
-        db_table = 'automated_payments'
+        managed = True
         verbose_name = 'Stripe Payment'
-        ordering = ["-created"]
+        ordering = ["-order_datetime"]
+        # db_table = 'automated_payments'
 
 
 #For Easypaisa_Payments:
@@ -49,8 +35,8 @@ class Easypaisa_Payment(models.Model):
     transaction_id = models.CharField(max_length=50, null=True,blank=True)
     order_datetime = models.DateTimeField(max_length=60, null=False , blank=False)
     customer_msidn = models.CharField(max_length=50, null=True,blank=True)
-    customer_email = models.EmailField()
-    amount_pkr = models.CharField(max_length=50, null=True,blank=True)
+    customer_email = models.EmailField(null=True,blank=True)
+    amoun = models.CharField(max_length=50, null=True,blank=True)
     status = models.CharField(max_length=50, null=True,blank=True)
     source = models.CharField(max_length=50, null=True , blank=True)
     credit_card = models.CharField(max_length=50, null=True,blank=True)
@@ -58,21 +44,21 @@ class Easypaisa_Payment(models.Model):
     fee_pkr = models.CharField(max_length=50, null=True,blank=True)
     fed_pkr = models.CharField(max_length=50, null=True,blank=True)
     error_reason = models.CharField(max_length=200, null=True,blank=True)
-    token_paid_datetime = models.CharField(max_length=50, null=True,blank=True)
+    token_paid_datetime = models.DateTimeField(max_length=60, null=False , blank=False)
         
     def __str__(self):
         return self.customer_email
    
     class Meta:
-        managed = False
-        db_table = 'easypaisa_payments'
+        managed = True
         verbose_name = 'Easypaisa Payment'
         ordering = ["-order_datetime"]
+        # db_table = 'easypaisa_payments'
 
 
+#For UBL_IPG_Payment:
 class UBL_IPG_Payment(models.Model):
-    
-    id = models.AutoField(primary_key=True)
+    # id = models.AutoField(primary_key=True)
     transaction_id = models.CharField(max_length=50, null=False,blank=False)
     customer_email = models.EmailField(null=True,blank=True)
     card_mask = models.CharField(max_length=100, null=True,blank=True)
@@ -91,7 +77,7 @@ class UBL_IPG_Payment(models.Model):
         return self.customer_email
 
     class Meta:
-        managed = False
-        db_table = 'ubl_ipg_payments'
+        managed = True
         verbose_name = "UBL IPG Payment"
         ordering = ["-order_datetime"]
+        # db_table = 'ubl_ipg_payments'
