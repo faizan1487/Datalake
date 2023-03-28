eddad0c88c7bfe9c2270380f4058b9968dbdd774
owais azad <azadm1808@gmail.com>
2023-03-28 16:04:06 +0500

    url update path

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import GetUserDetails
+from django.http import HttpResponse
 
 urlpatterns = [
+    path("", lambda req: HttpResponse(status=200)),
     path('users/', GetUserDetails.as_view(), name='user-list'),
     # path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
 ]
