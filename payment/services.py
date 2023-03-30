cba209ed403687987a9a7ea26bf7129a887975f5
faizan1487 <faizanahmed14887@gmail.com>
2023-03-30 15:10:45 +0500

    user and payment time filter bug fixed

--- payment/services.py
+++ payment/services.py
@@ -10,49 +10,44 @@ def stripe_pay(q, start_date, end_date, source):
     if start_date:
         pass
     else:
-        first_payment = Stripe_Payment.objects.first()
+        first_payment = Stripe_Payment.objects.last()
         date_time_obj = first_payment.order_datetime.strftime("%Y-%m-%d %H:%M:%S.%f%z")
         new_date_obj = datetime.strptime(date_time_obj, "%Y-%m-%d %H:%M:%S.%f")                                                                                      
-        start_date = new_date_obj + timedelta(days=20)
-        
+        start_date = new_date_obj + timedelta(days=20)      
     if end_date:
         pass
     else:
         pass
-        last_payment = Stripe_Payment.objects.last()
+        last_payment = Stripe_Payment.objects.first()
         date_time_obj = last_payment.order_datetime.strftime("%Y-%m-%d %H:%M:%S.%f%z")
         new_date_obj = datetime.strptime(date_time_obj, "%Y-%m-%d %H:%M:%S.%f")      
-        end_date = new_date_obj - timedelta(days=20)
-    
+        end_date = new_date_obj - timedelta(days=20)  
     if source:
         pass
     else:
-        source = ""
-        
+        source = ""      
     if q:
         queryset = Stripe_Payment.objects.filter(
             Q(customer_email__iexact=q) | Q(name__icontains=q)
             |Q(payment_id__iexact=q))
-        query_time = queryset.filter(Q(order_datetime__date__gte = end_date) & Q(order_datetime__date__lte = start_date))
+        query_time = queryset.filter(Q(order_datetime__date__gte = start_date) & Q(order_datetime__date__lte = end_date))
     else:
         queryset = Stripe_Payment.objects.all()
-        query_time = queryset.filter(Q(order_datetime__date__gte = end_date) & Q(order_datetime__date__lte = start_date))
-        
+        query_time = queryset.filter(Q(order_datetime__date__gte = start_date) & Q(order_datetime__date__lte = end_date))     
     return query_time
 
 def easypaisa_pay(q, start_date, end_date, source):
     if start_date:
         pass
     else:
-        first_payment = Easypaisa_Payment.objects.first()
+        first_payment = Easypaisa_Payment.objects.last()
         date_time_obj = first_payment.order_datetime.strftime("%Y-%m-%d %H:%M:%S.%f%z")
         new_date_obj = datetime.strptime(date_time_obj, "%Y-%m-%d %H:%M:%S.%f")                                                                                    
-        start_date = new_date_obj + timedelta(days=20)
-        
+        start_date = new_date_obj + timedelta(days=20)       
     if end_date:
         pass
     else:
-        last_payment = Easypaisa_Payment.objects.last()
+        last_payment = Easypaisa_Payment.objects.first()
         date_time_obj = last_payment.order_datetime.strftime("%Y-%m-%d %H:%M:%S.%f%z")
         new_date_obj = datetime.strptime(date_time_obj, "%Y-%m-%d %H:%M:%S.%f")
         end_date = new_date_obj - timedelta(days=20)
@@ -65,25 +60,28 @@ def easypaisa_pay(q, start_date, end_date, source):
         queryset = Easypaisa_Payment.objects.filter(
             Q(customer_email__iexact=q) | Q(product_name__icontains=q)
             |Q(order_id__iexact=q))
-        query_time = queryset.filter(Q(order_datetime__date__gte = end_date) & Q(order_datetime__date__lte = start_date))
+        query_time = queryset.filter(Q(order_datetime__date__lte = end_date) & Q(order_datetime__date__gte = start_date))
     else:
         queryset = Easypaisa_Payment.objects.all()
-        query_time = queryset.filter(Q(order_datetime__date__gte = end_date) & Q(order_datetime__date__lte = start_date))
+        query_time = queryset.filter(Q(order_datetime__date__lte = end_date) & Q(order_datetime__date__gte = start_date))
         
     return query_time
 
 def ubl_pay(q, start_date, end_date, source):
     if start_date==None:
-        first_payment = UBL_IPG_Payment.objects.first()
+        first_payment = UBL_IPG_Payment.objects.last()
         start_date = first_payment.order_datetime + timedelta(days=20) 
     else:
         pass  
         
     if end_date==None:
-        last_payment = UBL_IPG_Payment.objects.last()
+        last_payment = UBL_IPG_Payment.objects.first()
         end_date = last_payment.order_datetime - timedelta(days=20)
     else:
         pass
+    
+    print(start_date)
+    print(end_date)
     
     if source:
         pass
@@ -95,8 +93,8 @@ def ubl_pay(q, start_date, end_date, source):
         queryset = UBL_IPG_Payment.objects.filter(
             Q(customer_email__iexact=q) | Q(product_name__icontains=q)
             |Q(order_id__iexact=q))
-        time_query = queryset.filter(Q(order_datetime__date__gte=end_date) & Q(order_datetime__date__lte=start_date))
+        time_query = queryset.filter(Q(order_datetime__date__lte=end_date) & Q(order_datetime__date__gte=start_date))
     else:
         queryset = UBL_IPG_Payment.objects.all()
-        time_query = queryset.filter(Q(order_datetime__date__gte=end_date) & Q(order_datetime__date__lte=start_date))
+        time_query = queryset.filter(Q(order_datetime__date__lte=end_date) & Q(order_datetime__date__gte=start_date))
     return time_query
