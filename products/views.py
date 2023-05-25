4aaac7bb426172e72dbe17ea3db1cdd017eecd6c
faizan1487 <faizanahmed14887@gmail.com>
2023-05-25 12:49:47 +0500

    albaseer minor changes

--- products/views.py
+++ products/views.py
@@ -17,5 +17,8 @@ class AlnafiProduct(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
     def get(self,request):
-        data = Alnafi_Product.objects.values_list('name', flat=True)
-        return Response(data)
+        data = Alnafi_Product.objects.exclude(name__in=['test','test10']).values_list('name', flat=True)
+        words_to_remove = ['Quarterly', 'Monthly', 'Yearly', 'Half Yearly','Annual',' Half yearly','QUARTERLY','annual','Quaterly','TEST']
+
+        query_objects_without_words = [name for name in data if not any(word in name for word in words_to_remove)]
+        return Response(query_objects_without_words)
