c24ae553fe116ef3024a14ec0354fdee465c6eae
faizan1487 <faizanahmed14877@gmail.com>
2023-06-15 13:01:00 +0500

    minor changes in security app,scan model,post api created

--- security/models.py
+++ security/models.py
@@ -38,17 +38,17 @@ class Scan(models.Model):
         ('Internal', 'Internal'),
     ]
 
-    scan_type = models.CharField(max_length=100, choices=SCAN_TYPE_CHOICES)
-    scan_date = models.DateTimeField(auto_now_add=True)
-    severity = models.CharField(max_length=100, choices=SEVERITY_CHOICES)
-    remediation = models.TextField()
+    scan_type = models.CharField(max_length=100, choices=SCAN_TYPE_CHOICES,null=True,blank=True)
+    scan_date = models.DateTimeField(null=True,blank=True)
+    severity = models.CharField(max_length=100, choices=SEVERITY_CHOICES,null=True,blank=True)
+    remediation = models.TextField(null=True,blank=True)
     assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="user_scans")
-    scan_progress = models.CharField(max_length=100, choices=PROGRESS_CHOICES)
-    testing_method = models.CharField(max_length=100, choices=TESTING_METHOD_CHOICES)
-    target = models.CharField(max_length=100, choices=TARGET_CHOICES)
-    http_or_https = models.CharField(max_length=100, choices=HTTP_CHOICES)
-    application_type = models.CharField(max_length=100, choices=APPLICATION_TYPE_CHOICES)
-    findings_and_recommendations = models.TextField()
+    scan_progress = models.CharField(max_length=100, choices=PROGRESS_CHOICES,null=True,blank=True)
+    testing_method = models.CharField(max_length=100, choices=TESTING_METHOD_CHOICES,null=True,blank=True)
+    target = models.CharField(max_length=100, choices=TARGET_CHOICES,null=True,blank=True)
+    http_or_https = models.CharField(max_length=100, choices=HTTP_CHOICES,null=True,blank=True)
+    application_type = models.CharField(max_length=100, choices=APPLICATION_TYPE_CHOICES,null=True,blank=True)
+    findings_and_recommendations = models.TextField(null=True,blank=True)
     file_upload = models.FileField(upload_to="media/security/file_uploads",null=True, blank=True)  # For general file uploads
     poc = models.ImageField(upload_to='media/securitypoc_uploads/',null=True, blank=True)  # For image uploads (e.g., PoC photo)
 
--- security/views.py
+++ security/views.py
@@ -29,5 +29,5 @@ class CreateScan(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
