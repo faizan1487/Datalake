e8eac9a3f874df53dd64106c54e2550f4449dbde
faizan1487 <faizanahmed14877@gmail.com>
2023-06-15 13:08:15 +0500

    minor changes in security app,scan model,post api created

--- security/models.py
+++ security/models.py
@@ -49,7 +49,7 @@ class Scan(models.Model):
     http_or_https = models.CharField(max_length=100, choices=HTTP_CHOICES,null=True,blank=True)
     application_type = models.CharField(max_length=100, choices=APPLICATION_TYPE_CHOICES,null=True,blank=True)
     findings_and_recommendations = models.TextField(null=True,blank=True)
-    file_upload = models.FileField(upload_to="media/security/file_uploads",null=True, blank=True)  # For general file uploads
+    file_upload = models.FileField(upload_to="media/security/file_uploads/",null=True, blank=True)  # For general file uploads
     poc = models.ImageField(upload_to='media/securitypoc_uploads/',null=True, blank=True)  # For image uploads (e.g., PoC photo)
 
     def __str__(self):
