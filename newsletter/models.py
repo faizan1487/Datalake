6b2e475661bf57d71d8bf9f34e8a95ae28a9d0a6
faizan1487 <faizanahmed14877@gmail.com>
2023-07-13 13:54:02 +0500

    minor changes

--- newsletter/models.py
+++ newsletter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Newsletter(models.Model):
     first_name = models.CharField(max_length=100 , null=True , blank=True)
     phone = models.CharField(max_length=100 , null=True , blank=True)
-    email = models.EmailField(unique=True, null=True , blank=True)
+    email = models.EmailField(unique=True)
     erp_lead_id = models.CharField(max_length=255,blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
