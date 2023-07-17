66ac7d77126ff7d3c42c4a699c1148a56749441c
faizan1487 <faizanahmed14877@gmail.com>
2023-07-17 13:48:49 +0500

    thinkific enrollment signal created for erp

--- thinkific/signals.py
+++ thinkific/signals.py
@@ -1,11 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import requests
-from .models import Thinkific_User
+from .models import Thinkific_User, Thinkific_Users_Enrollments
 from requests.exceptions import RequestException
 from user.constants import COUNTRY_CODES
 
-@receiver(post_save, sender=Thinkific_User)
+@receiver(post_save, sender=Thinkific_Users_Enrollments)
 def send_lead_post_request(sender, instance, created, **kwargs):
     source='Thinkific'
     thinkific_user = usersignal(instance,source)
@@ -15,15 +15,16 @@ def send_lead_post_request(sender, instance, created, **kwargs):
 #agar product demo he to phir enrollment lead he
 
 def usersignal(instance,source):
-    post_save.disconnect(send_lead_post_request, sender=Thinkific_User)
+    post_save.disconnect(send_lead_post_request, sender=Thinkific_Users_Enrollments)
     # if instance.is_processing:
     #     return
 
     # print("instance", instance.user_enrollments.all())
     # print(enrollment[0])
     try:
-        enrollment = instance.user_enrollments.all()
-        if enrollment[0].course_name.startswith("demo"):
+        # enrollment = instance.user_enrollments.all()
+        user = instance.user_id
+        if instance.course_name.startswith("demo"):
             api_key = '2b4b9755ecc2dc7'
             api_secret = '8d71fb9b172e2aa'
             headers = {
@@ -32,7 +33,7 @@ def usersignal(instance,source):
                 "Accept": "application/json",
             }    
 
-            country_code = getattr(instance, 'country', "Unknown")
+            country_code = getattr(user, 'country', "Unknown")
             country_name = None
 
             if country_code:
@@ -42,17 +43,17 @@ def usersignal(instance,source):
                         break
 
             data = {
-                    "first_name": instance.first_name or None,
-                    "last_name": instance.last_name if hasattr(instance, 'last_name') else None,
-                    "email_id": instance.email or None,
-                    "mobile_no": instance.phone if hasattr(instance, 'phone') else None,
+                    "first_name": user.first_name or None,
+                    "last_name": user.last_name if hasattr(user, 'last_name') else None,
+                    "email_id": user.email or None,
+                    "mobile_no": user.phone if hasattr(user, 'phone') else None,
                     "country": country_name,
                     "source": source,
-                    "product_name": enrollment[0].course_name if hasattr(enrollment[0], 'course_name') else None,
+                    "product_name": instance.course_name if hasattr(instance, 'course_name') else None,
                     # Add other fields from the Main_User model to the data dictionary as needed
                 }
             
-            url = f'https://crm.alnafi.com/api/resource/Lead?fields=["name","email_id"]&filters=[["Lead","email_id","=","{instance.email}"]]'
+            url = f'https://crm.alnafi.com/api/resource/Lead?fields=["name","email_id"]&filters=[["Lead","email_id","=","{user.email}"]]'
             response = requests.get(url, headers=headers)
             lead_data = response.json()
             # print(lead_data['data'])
@@ -61,9 +62,9 @@ def usersignal(instance,source):
             # print(already_existed)
             if already_existed:
                 response = requests.put(url, headers=headers, json=data)
-                instance.erp_lead_id = lead_data['data'][0]['name']
+                user.erp_lead_id = lead_data['data'][0]['name']
                 print("lead updated")
-                instance.save(update_fields=['erp_lead_id'])
+                user.save(update_fields=['erp_lead_id'])
             else:
                 print("in else")
                 post_url = 'https://crm.alnafi.com/api/resource/Lead'
@@ -75,13 +76,12 @@ def usersignal(instance,source):
                     erp_lead_id = lead_data['data']['name']
                     if erp_lead_id:
                         print("lead id exists")
-                        instance.erp_lead_id = erp_lead_id
-                        instance.save(update_fields=['erp_lead_id'])
-                        print("Lead created successfully!")
-                        
-            post_save.connect(send_lead_post_request, sender=Thinkific_User)
+                        user.erp_lead_id = erp_lead_id
+                        user.save(update_fields=['erp_lead_id'])
+                        print("Lead created successfully!")   
     except Exception as e:
-        print('Error occurred while creating lead:')      
-        print('Error:', response.status_code)
-        print('Error:', response.text)
+        # print('Error occurred while creating lead:')      
+        # print('Error:', response.status_code)
+        # print('Error:', response.text)
         print(e)
+    post_save.connect(send_lead_post_request, sender=Thinkific_Users_Enrollments)
