1573a5996c409284088fd87c21092ad5b5c3ef7e
faizan1487 <faizan14877@gmail.com>
2023-08-17 11:48:18 +0500

    product analysis api fixed

--- payment/services.py
+++ payment/services.py
@@ -245,18 +245,18 @@ def search_payment(export, q, start_date, end_date, plan, request, url, product,
 
 
 def get_USD_rate():
-    usd_details = cache.get("usd_details")
-    if usd_details:
+    # usd_details = cache.get("usd_details")
+    # if usd_details:
         # print(usd_details)
         # print("usd_details", usd_details["PKR"])
-        return json.loads(usd_details)
+        # return json.loads(usd_details)
     usd_details = {}
     url = f"https://v6.exchangerate-api.com/v6/{settings.EXCHANGE_RATE_API_KEY}/latest/USD"
     response = requests.get(url).json()
     usd_details["PKR"] = response["conversion_rates"]["PKR"]
     usd_details["USD"] = response["conversion_rates"]["USD"]
 
-    cache.set("usd_details", json.dumps(usd_details), 60*120)
+    # cache.set("usd_details", json.dumps(usd_details), 60*120)
     # print("usd_details",usd_details)
     return usd_details
 
