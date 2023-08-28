17166b6dd8e3dc10be75aa5d871e811d443c0e7f
faizan1487 <faizan14877@gmail.com>
2023-08-28 11:07:47 +0500

    chapters api modified

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GetFeedbacks, GetFeedbackAnswers,GetCoursesNames, GetCoursesChapters
+from .views import GetFeedbacks, GetFeedbackAnswers,GetCoursesNames, GetCoursesChapters, GetFeedbackProgress
 
 urlpatterns = [
     path("getfeedbacks/", GetFeedbacks.as_view(), name='get-feedbacks'),
     path("getfeedbacksasnwers/", GetFeedbackAnswers.as_view(), name='get-feedbacks-answers'),
+    path("getfeedbackprogress/", GetFeedbackProgress.as_view(), name='get-feedbacks-progress'),
     path("getcoursesnames/", GetCoursesNames.as_view(), name='get-courses-names'),
     path("getchaptersnames/", GetCoursesChapters.as_view(), name='get-chapters-names'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -78,6 +78,45 @@ class GetFeedbackAnswers(APIView):
         return Response(feedbacks)
 
 
+class GetFeedbackProgress(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        # q = self.request.GET.get('q', None) or None
+        start_date = self.request.GET.get('start_date', None) or None
+        end_date = self.request.GET.get('end_date', None) or None
+        email = self.request.GET.get('email', None) or None
+        course = self.request.GET.get('course', None) or None
+        chapter = self.request.GET.get('chapter', None) or None
+        feedbacks = FeedbackAnswers.objects.all().values("user_email","feedback_question_id__course_name","feedback_question_id__chapter_name","question_answer","created_at")
+
+        if email:
+            feedbacks = feedbacks.filter(user_email=email)        
+        if course:
+            feedbacks = feedbacks.filter(feedback_question_id__course_name=course)
+        if chapter:
+            feedbacks = feedbacks.filter(feedback_question_id__chapter_name=chapter)
+
+        # Extract the 'created_at' values
+        created_at_values = [item['created_at'] for item in feedbacks]
+
+        # Find the minimum and maximum 'created_at' valus    
+        if not start_date:
+            start_date = min(created_at_values)
+        if not end_date:
+            end_date = max(created_at_values)
+         
+
+        feedbacks = feedbacks.filter(Q(created_at__date__lte=end_date) & Q(created_at__date__gte=start_date))
+
+        # print(feedbacks)
+        total_yes += 0
+        total_no += 0
+        for i in feedbacks:
+            print(i['question_answer'])
+
+        return Response(feedbacks)
+
+
 class GetCoursesNames(APIView):
     permission_classes = [IsAuthenticated]
     def get(self,request):
@@ -96,13 +135,15 @@ class GetCoursesChapters(APIView):
             queryset = queryset.filter(course_name=course)
     
         # Create a defaultdict to group chapter names by course names
-        grouped_data = defaultdict(list)
-
+        # grouped_data = defaultdict(list)
+        chapters = []
         # Group the chapter names under respective course names
         for item in queryset:
             course_name = item['course_name']
             chapter_name = item['chapter_name']
-            grouped_data[course_name].append(chapter_name)
+            # grouped_data[course_name].append(chapter_name)
+            chapters.append(chapter_name)
 
-        return Response(grouped_data)
+        # return Response(grouped_data)
+        return Response(chapters)
 
