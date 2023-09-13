16c09c142435813c26fcaff852edfe66fd9f466c
SameerShiekh77 <muhammadsameer.css@gmail.com>
2023-09-13 18:29:06 +0500

    import export field added

--- secrets_api/admin.py
+++ secrets_api/admin.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 from secrets_api.models import LastSecretApiUsing, AllSecretsApi
-from albaseer.settings import DEBUG
+from albaseer.settings import DEBUG\
+from import_export.admin import ImportExportModelAdmin,ExportActionMixin
 
-class LastSecretApiUsingAdmin(admin.ModelAdmin):
+class LastSecretApiUsingAdmin(ImportExportModelAdmin,ExportActionMixin,admin.ModelAdmin):
     list_display = ['user_name','turn_number','api_key','secret_key']
     search_fields = ['user_name','turn_number','api_key','secret_key']
     list_filter = ['turn_number']
     # if not DEBUG:
-    #     readonly_fields = list_display
+    readonly_fields = list_display
     
 
 admin.site.register(LastSecretApiUsing,LastSecretApiUsingAdmin)
 
-class AllSecretsApiAdmin(admin.ModelAdmin):
+class AllSecretsApiAdmin(ImportExportModelAdmin,ExportActionMixin,admin.ModelAdmin):
     list_display = ['user_name','turn_number','api_key','secret_key']
     search_fields = ['user_name','api_key','secret_key']
     list_filter = ['turn_number']
     # if not DEBUG:
-    #     readonly_fields = list_display
+    readonly_fields = list_display
         
 admin.site.register(AllSecretsApi,AllSecretsApiAdmin)
