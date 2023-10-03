fc2953dd96cabfabdebc73b6d83bd069d9bd278e
faizan1487 <faizan14877@gmail.com>
2023-10-03 14:45:52 +0500

    new alnafi product model added and three fields added in main product

--- products/admin.py
+++ products/admin.py
@@ -32,8 +32,12 @@ admin.site.register(IslamicAcademy_Product, IslamicAcademyProductAdmin)
 
 # FOR MAIN PRODUCTS COMBINE PRODUCT ALL TABLE IN ONE:
 class MainProductAdmin(ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin):
-    list_display = ("id", "source", "product_name", "product_slug", "created_at", "modified_at", "product_type", "product_plan", "amount_pkr", "amount_usd", "amount_gbp", "old_amount_pkr", "old_amount_usd", "legacy_fee_pkr", "legacy_fee_usd",
-                    "legacy_available", "qarz_product", "qarz_fee_pkr", "qarz_fee_usd", "lms_id", "product_language", "has_legacy_version", "is_certificate_product", "allow_coupon", "courses", "image", "bundle_ids", "status", "description", "stock_status")
+    list_display = ("id", "source", "product_name", "product_slug", "created_at", "modified_at", "product_type", "product_plan", "amount_pkr", 
+                    "amount_usd", "amount_gbp", "old_amount_pkr", "old_amount_usd", "legacy_fee_pkr", "legacy_fee_usd",
+                    "legacy_available", "qarz_product", "qarz_fee_pkr", "qarz_fee_usd", "lms_id", "product_language", 
+                    "has_legacy_version", "is_certificate_product", "allow_coupon", "courses", "image", 
+                    "bundle_ids", "status", "description", "stock_status","duration","discount_applied_pkr","discount_applied_usd")
+    
     search_fields = ("product_name", "product_slug")
     list_filter = ("created_at", "source", "product_type", "product_plan",
                    "product_language", "allow_coupon", "amount_pkr", "amount_usd")
@@ -56,13 +60,10 @@ class CourseAdmin(ImportExportModelAdmin, admin.ModelAdmin):
 
 admin.site.register(Course, CourseAdmin)
 
-#For Al-Nafi New Main Site:
+# For Al-Nafi New Main Site:
 class NewAlnafiProductAdmin(ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin):
-    list_display = ('id', 'product_type', 'product_slug', 'plan', 'name', 'bundle_Ids', 'amount_pkr', 'amount_usd', 'amount_gbp', 'old_amount_pkr', 'old_amount_usd', 'legacy_fee_pkr', 'legacy_fee_usd',
-                    'legacy_available', 'qarz_product', 'qarz_fee_pkr', 'qarz_fee_usd', 'lms_id', 'language', 'created_at', 'has_legacy_version', 'is_certificate_product', 'allow_coupon', 'courses')
-    search_fields = ("id", "name", "product_slug", "language",
-                     "bundle_Ids", "plan", "product_type")
-    list_filter = ('created_at', "language", "product_type", "plan")
+    list_display = ('product_name', 'slug', 'product_plan', 'product_type', 'amount_pkr', 'amount_usd', 'duration', 'discount_applied_pkr', 'discount_applied_usd', 'price_gbp', 'alnafi_product_id', 'product_language', 'is_certificate_product', 'bundle_ids', "created_at")
+    search_fields = ('slug', 'product_plan', 'product_type', 'alnafi_product_id', 'product_name', 'product_language', 'is_certificate_product', 'bundle_ids')
+    list_filter = ('product_type', 'product_plan', 'product_language', 'is_certificate_product', 'created_at', 'amount_pkr', 'amount_usd')
 
-
-admin.site.register(New_Alnafi_Product, AlnafiProductAdmin)
+admin.site.register(New_Alnafi_Product, NewAlnafiProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from trainers.models import *
+from datetime import timezone, datetime
 # Create your models here.
 
 # FOR AL-NAFI MAIN SITE PRODUCT:
@@ -112,6 +113,9 @@ class Main_Product(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
     description = models.CharField(max_length=255, null=True, blank=True)
     stock_status = models.CharField(max_length=20, default='instock')
+    duration = models.CharField(max_length=50, null=True, blank=True)
+    discount_applied_pkr = models.CharField(max_length=100, blank=True, null=True)
+    discount_applied_usd = models.CharField(max_length=100, blank=True, null=True)
     
     def __str__(self):
         return f"{self.product_name}"
@@ -271,39 +275,24 @@ class Track(models.Model):
 
 # For Al-Nafi New Main Site Product:
 class New_Alnafi_Product(models.Model):
-    id = models.IntegerField(primary_key=True)
-    image = models.CharField(max_length=255, null=True, blank=True)
+    product_name = models.CharField(unique=True, max_length=255, blank=True, null=True)
+    slug = models.CharField(max_length=255, null=True, blank=True)
     product_type = models.CharField(max_length=255, null=True, blank=True)
-    product_slug = models.CharField(max_length=255, null=True, blank=True)
-    plan = models.CharField(max_length=255, null=True, blank=True)
-    name = models.CharField(max_length=255, null=True, blank=True)
-    bundle_Ids = models.CharField(max_length=255, null=True, blank=True)
-    amount_pkr = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0)
-    amount_usd = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0)
-    amount_gbp = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0)
-    old_amount_pkr = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0)
-    old_amount_usd = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0)
-    legacy_fee_pkr = models.IntegerField(default=0, null=True, blank=True)
-    legacy_fee_usd = models.IntegerField(default=0, null=True, blank=True)
-    legacy_available = models.BooleanField(default=False)
-    qarz_product = models.BooleanField(default=False)
-    qarz_fee_pkr = models.IntegerField(null=True, blank=True)
-    qarz_fee_usd = models.IntegerField(null=True, blank=True)
-    lms_id = models.CharField(max_length=255, null=True, blank=True)
-    language = models.CharField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    has_legacy_version = models.BooleanField(default=False)
+    product_plan = models.CharField(max_length=255, null=True, blank=True)
+    amount_pkr = models.CharField(max_length=50, null=True, blank=True)
+    amount_usd = models.CharField(max_length=50, null=True, blank=True)
+    duration = models.CharField(max_length=50, null=True, blank=True)
+    discount_applied_pkr = models.CharField(max_length=100, blank=True, null=True)
+    discount_applied_usd = models.CharField(max_length=100, blank=True, null=True)
+    price_gbp = models.CharField(max_length=100, blank=True, null=True)
+    alnafi_product_id = models.CharField(max_length=100, null=True, blank=True)
+    product_language = models.CharField(max_length=255, blank=True, null=True)
     is_certificate_product = models.BooleanField(default=False)
-    allow_coupon = models.BooleanField(default=False)
-    courses = models.CharField(max_length=255, null=True, blank=True)
+    bundle_ids = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(default=datetime.now, null=True, blank=True)
 
-    def __str__(self):
-        return f"name {self.name}"
+    def _str_(self):
+        return f"name {self.product_name}"
 
     class Meta:
         managed = True
