d89c37cfde7d340b07924130c2009d69b7a6af6f
faizan1487 <faizan14877@gmail.com>
2023-10-24 15:42:09 +0500

    new field added in alnafi user model

--- user/models.py
+++ user/models.py
@@ -92,6 +92,7 @@ class Main_User(models.Model):
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+    academy_demo_access = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     
     def __str__(self):
