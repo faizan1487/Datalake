e3e57ba13909b05da7c0d89a500aa01efdf63d45
faizan1487 <faizan14877@gmail.com>
2023-12-16 16:10:18 +0500

    minor changes

--- payment/renewal_leads.py
+++ payment/renewal_leads.py
@@ -3,7 +3,8 @@ import requests
 import pandas as pd
 
 def handle():
-    url = "https://fc37-2400-adc1-175-d500-5cbe-3fd2-50b-5daa.ngrok-free.app/api/v1.0/enrollments/expiry_date_user/"
+    # url = "https://fc37-2400-adc1-175-d500-5cbe-3fd2-50b-5daa.ngrok-free.app/api/v1.0/enrollments/expiry_date_user/"
+    url = "stage-api-al-baseer.alnafi.com/api/v1.0/enrollments/expiry_date_user/"
     response = requests.get(url)
     data = response.json()
 
