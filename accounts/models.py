866889a2a020815ada0f325f95e6d1ec1abd4e72
faizan1487 <faizan14877@gmail.com>
2023-12-18 15:33:11 +0500

    changes in expense model

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from user.models import User
 
 class Expense(models.Model):
-    subject = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_length=3)  # Assuming 3-letter currency code (e.g., USD, EUR)
+    subject = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=100, decimal_places=2)
+    currency = models.CharField(max_length=10)  # Assuming 3-letter currency code (e.g., USD, EUR)
     created_at = models.DateTimeField(auto_now_add=True)
     month = models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
