0683c85c3be4485596cbb0c61c02c352b4c1b698
faizan1487 <faizan14877@gmail.com>
2023-12-18 16:34:54 +0500

    minor changes

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from user.models import User
 
 class Expense(models.Model):
     subject = models.CharField(max_length=200)
-    amount = models.DecimalField(max_digits=60, decimal_places=2)
+    amount = models.DecimalField(max_digits=55, decimal_places=2)
     currency = models.CharField(max_length=10)  # Assuming 3-letter currency code (e.g., USD, EUR)
     created_at = models.DateTimeField(auto_now_add=True)
     month = models.CharField(max_length=20)
