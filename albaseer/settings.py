cd30897b0e67b416e589184f2473d9aa1ac09487
faizan1487 <faizan14877@gmail.com>
2023-12-22 15:38:41 +0500

    minor changes

--- albaseer/settings.py
+++ albaseer/settings.py
@@ -129,7 +129,7 @@ INSTALLED_APPS = [
 ]
 
 CRONJOBS = [
-    ('* * * * *', 'payment.renewal_leads.handle'),  # Run every 24 hours
+    ('0 0 * * *', 'payment.renewal_leads.handle'),  # Run every 24 hours
 ]
 
 # '0 0 * * *',
